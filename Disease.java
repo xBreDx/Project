3eb7a5ad43827078d842acb79dc14ae91172dca4
xBreDx <161073339+xBreDx@users.noreply.github.com>
2024-02-23 14:07:51 -0500

    Update Disease.java

--- Disease.java
+++ Disease.java
@@ -1,1 +1,39 @@
+public class Disease {
+  private String Name;
+  private String Symptoms;
+  private String Treatment;
+  
 
+public Disease (String name, String symptoms, String treatment) {
+  this.Name = name;
+  this.Symptoms = symptoms;
+  this.Treatment = treatment;
+}
+  public Disease() {
+    
+  }
+   public String getName() {
+    return this.Name;
+  }
+
+  public String getSymptoms() {
+    return this.Symptoms;
+  }
+
+  public String getTreatment(){
+    return this.Treatment;
+  }
+
+  public void setName(String name) {
+    this.Name = name;
+  }
+
+  public void setSymptoms(String symptoms) {
+    this.Symptoms = symptoms;
+  }
+
+  public void setTreatment(String treatment){
+    this.Treatment = treatment;
+  }
+  
+}
